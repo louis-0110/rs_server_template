2e154ecb4d0ecb69c3b3ca4247e516d48f85f612
gaoluo <gl574169815@gmail.com>
2024-05-15 19:40:48 +0800

    add evn and tarcing log

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,35 +1,56 @@
 use axum::{routing::get, Json};
 use reqwest::StatusCode;
 use serde_json::{json, Value};
-use tracing::info;
+use tracing::{debug, error, info, trace, warn};
 use tracing_subscriber::fmt::time::ChronoLocal;
 
 #[tokio::main]
 async fn main() {
+    // init env
+    dotenvy::dotenv().ok();
+
     // log
     let timer = ChronoLocal::new("%Y-%m-%d %H:%M:%S.%3f".into());
     tracing_subscriber::fmt()
+        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
         .with_ansi(true)
         .with_line_number(true)
         .with_timer(timer)
         .init();
 
-    let app = axum::Router::new().route("/", get(resp_json));
+    let app = axum::Router::new().route("/:id", get(resp_json));
 
-    let server = tokio::net::TcpListener::bind("127.0.0.1:3000")
+    let server = tokio::net::TcpListener::bind(std::env::var("ADDR").unwrap())
         .await
         .expect("Failed to bind address");
-    info!("Server started on http://127.0.0.1:3000");
+
+    let address = server.local_addr().unwrap();
+    info!("Server started on {}", address);
     axum::serve(server, app)
         .await
         .expect("Failed to start server");
 }
 
-async fn resp_json() -> (StatusCode, Json<Value>) {
+async fn resp_json(id: axum::extract::Path<String>) -> (StatusCode, Json<Value>) {
+    let __id: isize;
+    match id.parse::<isize>() {
+        Ok(res) => __id = res,
+        Err(_) => __id = 0,
+    }
+    let code: StatusCode;
+    if __id > 10 {
+        code = StatusCode::FORBIDDEN;
+    } else {
+        code = StatusCode::OK;
+    }
+    warn!("id: {}", __id);
+    trace!("id: {}", __id);
+    error!("id: {}", __id);
+    debug!("id: {}", __id);
     (
-        StatusCode::FORBIDDEN,
+        code,
         Json(json!({
-            "status": "ok",
+            "id": __id,
             "message": "Hello, World!"
         })),
     )
